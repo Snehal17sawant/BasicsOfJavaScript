f94dbef847812f04935fe103fdac2808c9546f15
Snehal17sawant <snehalsawant569@gmail.com>
2021-09-04 17:22:35 +0530

    Comparison operators

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-// Assignment operators
-let Name = "Snehal"
-let a = 19
+// Comparison  operators
 
-// a += Name
-// a /= 7
-// a*=
-//a **=
+const a = 21
+const b = 7
+const c = 21
+
+console.log("a > b", a > b)
+console.log(a < b)
+console.log(a == c)
+console.log(a >= c)
+
+console.log("-------------------")
+//----------------------------
+
+const e = 11
+const d = "11"
+
+console.log(e == d)
+console.log(e === d)
+// e==d && number==string
