450d4796687c2af3493565c64c9c3566612a1180
Snehal17sawant <snehalsawant569@gmail.com>
2021-09-04 10:52:10 +0530

    Console basics

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("hey this is my first piece of javascript code!!!")
+
+console.log(13445767545342)
+
+console.log( { name: "Snehal"})
+
+console.log(true)
+console.log(false)
+
+console.log(false, 234243, "string", { name: "Sawant"})
+
+console.warn("This is warning")
+
+console.error("This is an error")
+
