bab089679f96ccd8f3ff912dc901403be8d31498
Snehal17sawant <snehalsawant569@gmail.com>
2021-09-04 17:38:52 +0530

    Logical operators

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-// Comparison  operators
+// Logical operators
 
-const a = 21
-const b = 7
-const c = 21
+const a = 2
+const b = 3
+const c = 35
 
-console.log("a > b", a > b)
-console.log(a < b)
-console.log(a == c)
-console.log(a >= c)
+console.log(c > a && c < b)
 
-console.log("-------------------")
-//----------------------------
+// console.log("a == b", a == b)
+// console.log("a != b", a != b)
 
-const e = 11
-const d = "11"
+// console.log("Truth table for AND")
+// console.log(" ")
 
-console.log(e == d)
-console.log(e === d)
-// e==d && number==string
+// console.log("false && false", false&&false)
+// console.log("false && true", false && true)
+// console.log("true && false", true && false)
+// console.log("true && true", true && true)
+// console.log(" ")
+
+// console.log("Truth table for OR")
+// console.log(" ")
+
+// console.log("false || false", false || false)
+// console.log("false || true", false || true)
+// console.log("true || false", true || false)
+// console.log("true || true", true || true)
