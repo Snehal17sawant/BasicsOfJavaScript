57ec806362d52d2095c13754c4ac50e8106ab66f
Snehal17sawant <snehalsawant569@gmail.com>
2021-09-04 17:07:41 +0530

    Assignment operators

--- app.js
+++ app.js
@@ -1,33 +1,8 @@
-// Arithmetic operators
+// Assignment operators
+let Name = "Snehal"
+let a = 19
 
-const a = 2
-const b = 15
-let c = 11
-
-console.log(a + b)
-
-console.log(a - b)
-
-console.log(a * b)
-
-console.log(a / b)
-
-console.log(a ** b)
-
-console.log(b % a)
-
-console.log("_________________________________________")
-
-//c += 1
-// c++
-// c += 1 equivalent to c++
-// c = c + 1 => 11 + 1 = 12
-
-//c *= 5
-// c operator is equivalent to c = c operator numbar
-// c = c * 5 = 11 * 5 = 55
-
-// c /= 11
-// c = c / 11 = 11 / 11 = 1
-
-console.log(c)
+// a += Name
+// a /= 7
+// a*=
+//a **=
